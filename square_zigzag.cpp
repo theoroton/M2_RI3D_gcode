05a8a62e8baf47c716260c781c86df6b97aa7c6d
Anna Sushko <sushko1u@aig-ats-240-l04.fst.site.univ-lorraine.fr>
2022-02-10 18:21:23 +0100

    Square zigzag done

--- square_zigzag.cpp
+++ square_zigzag.cpp
@@ -37,7 +37,7 @@ double compute_deltaE(double L)
     double VE = L * tau * nw;
     
     //Calcul deltaE
-    double deltaE = VE / (M_PI * (d*d / 4));
+    double deltaE = VE / (M_PI * (d*d / 4));//VE*4/(M_PI*d*d);//
 
     //Retourne deltaE
     return deltaE;
@@ -84,10 +84,10 @@ int main () {
     //Nombre de couches
     int nb_layers = 25;
     //Largeur côté carré
-    float L = 20;
+    float L = 40;
     //Coordonnées de départ
-    float X_start = 20.0;
-    float Y_start = 20.0;
+    float X_start = 80.0;
+    float Y_start = 50.0;
     //Hauteur de départ
     float Z = tau;
 
@@ -104,19 +104,36 @@ int main () {
 
     //Ajout des points à la liste des points
     points.push_back(p1);
-    points.push_back(p2);
-    points.push_back(p3);
     points.push_back(p4);
+    points.push_back(p3);
+    points.push_back(p2);
     points.push_back(p1);
+    //points.push_back(p1);
 
     //Calcul de deltaE (deltaE constant car on a un carré)
     float deltaE = compute_deltaE(L);
     //Initialisation E
-    float E = 0;
+    float E = 0;//0;
 
     //Déplacement au premier point
-    gcode_file << "G0 X" << p1.x << " Y" << p1.y << " Z" << Z << " F" << F_G0 << ";" << "\n"; 
-
+    int xC = 40;
+    int yC = 10;
+    gcode_file << "G0 X"<<xC<<" Y"<<yC<<" Z" << Z << " F" << F_G0 << ";" << "\n"; 
+    
+    for (int i = 1; i < 10; i++) {
+        if(i%2 != 0) {
+            yC += 10;
+        } else {
+            xC += 10;
+            yC -= 10;
+        }
+        E += deltaE;
+        gcode_file << "G1 X" << xC << " Y" << yC << " E" << E << " F" << F_G1 << "\n";
+    }
+    deltaE = 2*compute_deltaE(L);
+    E += deltaE;
+    gcode_file << "G1 X" << xC << " Y" << yC << " E" << E << " F" << F_G1 << "\n";
+    
     //Pour chaque point du carré
     for (int i = 0; i < points.size(); i++) {
         Point p = points[i];       
@@ -124,36 +141,45 @@ int main () {
         E += deltaE;
         //Déplacement G1 à ce point
         gcode_file << "G1 X" << p.x << " Y" << p.y << " E" << E << " F" << F_G1 << "\n";
+ 
     }
 
     //-----------Zigzag-----------
 
     //Calcul des points du carré
-    p1.x = X_start+tau;     p1.y = Y_start-tau;
-    p4.x =  X_start + L-tau;     p4.y = Y_start + L+tau;
+    //p1.x = X_start;     p1.y = X_start;
+    p4.x =  X_start + L-tau;     p4.y = X_start + L+tau;
 
-    Point curr = {p1.x, p1.y};
+    Point curr = {p1.x+2*tau, p1.y+2*tau};
     //Déplacement au premier point interieur
     gcode_file << "G0 X" << curr.x << " Y" << curr.y << " Z" << Z << " F" << F_G0 << ";" << "\n"; 
-    float L_interieur = L;
+    float L_interieur = L-3*tau;
     int counter = 0;
-    int iter_max = L_interieur/tau;
-    E = 0;
-    while(counter != iter_max){
-    //while(curr.x != p4.x && curr.y != p4.y) {
-        if(counter == 0) {
-            curr.y +=L_interieur - tau;
-        } else {
-            curr.y = (counter%2 == 0) ? curr.y+L_interieur - 2*tau : curr.y-L_interieur + 2*tau;
+    //int iter_max = (L_interieur/tau)+1;
+    //E = 0;
+    //while(counter != iter_max){
+        
+    while(curr.x < p4.x && curr.y < p4.y) {
+        //curr.x += tau;
+        /*if(counter == 0) {
+            curr.y = curr.y+L_interieur - 3*tau;
         }
+        else {
+        */    curr.y = (counter%2 == 0) ? curr.y+L_interieur - tau : curr.y-L_interieur + tau;
+       //}
+        /*if(counter == 1) {
+            curr.y -=tau;
+        } */
         //Augmente E par deltaE
         E += deltaE;
         //Déplacement G1 à ce point
         gcode_file << "G1 X" << curr.x << " Y" << curr.y << " E" << E << " F" << F_G1 << "\n";
-        if(curr.x != p4.x && curr.y != p4.y) {
+        curr.x+=2*tau;
+        gcode_file << "G1 X" << curr.x << " Y" << curr.y << " E" << E << " F" << F_G1 << "\n";
+        /*if(curr.x != p4.x && curr.y != p4.y) {
             curr.x += tau;
             gcode_file << "G1 X" << curr.x << " Y" << curr.y << " E" << E << " F" << F_G1 << "\n";
-        }
+        }*/
         counter++;
     }
 
